6e1c24572bbb5d6b3ef03ab63b51a7146623b86b
Besufikad Hosiso <besuyeboss@gmail.com>
2026-04-14 14:33:52 +0300

    refactor(dataFetching): fetch all the comments, posts and full user infos

--- one-react-state/src/components/useEffect.jsx
+++ one-react-state/src/components/useEffect.jsx
@@ -4,29 +4,66 @@ import { useState, useEffect } from 'react';
 function UseEffect() {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState(null);
+  const [reType, setReType] = useState("posts");
   useEffect(() => {
     //  Fetch user name and email from an api
-    fetch('https://jsonplaceholder.typicode.com/users/2')
+    fetch(`https://jsonplaceholder.typicode.com/${reType}`)
     .then((response) => response.json())
     .then((data) => {
       setData(data);
       setLoading(false);
+      console.log(data);
     })
-  }, []);
+  }, [reType]);
 
   if (loading) return <p>Loading...</p>
   if (!data) return <p>404 User Not Found</p>;
   
   return (
-    // User Profile Card with email username and button
-    <div className='w-full max-w-md max-mx-4 mx-auto bg-white p-6 rounded-lg shadow-md mt-6 text-gray-800'>
-      <h2 className='text-xl font-bold mb-2'>User Profile</h2>
-      <p className='mb-4'>Name: {data?.name}</p>
-      <p className='mb-4'>Email: {data?.email}</p>
-      <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
-        Update Profile
-      </button>
+    <div className='w-full flex flex-col items-center justify-center gap-4 py-4'>
+    <div className='flex flex-row items-center justify-center gap-4 mx-auto mt-4'>
+      <button onClick={() => setReType("posts")} className='py-3 px-5 m-0 bg-gray-300 rounded-full cursor-pointer'>posts</button>
+      <button onClick={() => setReType("comments")} className='py-3 px-5 m-0 bg-gray-300 rounded-full cursor-pointer'>comments</button>
+      <button onClick={() => setReType("users")} className='py-3 px-5 m-0 bg-gray-300 rounded-full cursor-pointer'>users</button>
     </div>
+    <h1 className='text-center mt-4 text-2xl font-bold'>{reType}</h1>
+   {
+    reType === "posts" ? (
+      <div className='w-full grid col-1 md:grid-cols-2 lg:grid-cols-3 gap-4 items-center justify-center gap-4 py-4'>
+        {data.map((item) => (
+          <div key={item.id} className='w-full max-w-md bg-gray-100 rounded-lg p-4 shadow-md'>
+            <h2 className='text-xl font-bold mb-2'>{item.title}</h2>
+            <p className='text-gray-700'>{item.body}</p>
+          </div>
+        ))}
+      </div>
+    ) : reType === "comments" ? (
+      <div className='w-full grid col-1 md:grid-cols-2 lg:grid-cols-3 gap-4 items-center justify-center gap-4 py-4'>
+        {data.map((item) => (
+          <div key={item.id} className='w-full max-w-md bg-gray-100 rounded-lg p-4 shadow-md flex flex-1 flex-col items-start justify-start gap-2'>
+            <h2 className='text-xl font-bold mb-2'>{item.name}</h2>
+            <p className='text-gray-700'>{item.body}</p>
+          </div>
+        ))}
+      </div>
+    ) : reType === "users" ? (
+      <div className='w-full grid col-1 md:grid-cols-2 lg:grid-cols-3 gap-4 items-center justify-center gap-4 py-4'>
+        {data.map((item) => (
+          <div key={item.id} className='w-full max-w-md bg-gray-100 rounded-lg p-4 shadow-md'>
+            <h2 className='text-xl font-bold mb-2'>Name: {item.name}</h2>
+            <p className='text-gray-700'>Email: {item.email}</p>
+            <p className='text-gray-700'>Username: {item.username}</p>
+            <p className='text-gray-700'>Phone: {item.phone}</p>
+            <p className='text-gray-700'>Street: {item.address ? item.address.street : 'N/A'}</p>
+            <p className='text-gray-700'>City: {item.address ? item.address.city : 'N/A'}</p>
+            <p className='text-gray-700'>Zipcode: {item.address ? item.address.zipcode : 'N/A'}</p>
+          </div>
+        ))}
+      </div>
+    ) : null
+   }
+    </div>
+    
   )
 }
 
